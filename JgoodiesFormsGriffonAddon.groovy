aacaa150c21b5960032f023597aaadaeddea921a
aalmiray <aalmiray@bb902e69-0157-0410-a63b-ba7c3dd83122>
2012-01-01 12:54:34 +0000

    Full repo update before adding arifact descriptors

--- JgoodiesFormsGriffonAddon.groovy
+++ JgoodiesFormsGriffonAddon.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 the original author or authors.
+ * Copyright 2009-2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,10 +19,10 @@ import griffon.jgoodies.*
 /**
  * @author Andres Almiray
  */
-class JGoodiesFormsGriffonAddon {
+class JgoodiesFormsGriffonAddon {
     def factories = [
-        "form": new FormFactory(),
-        "formLayout": new FormLayoutFactory(),
-        "cc": new CellConstraintsFactory()
+        form: new FormFactory(),
+        formLayout: new FormLayoutFactory(),
+        cc: new CellConstraintsFactory()
     ]
 }
